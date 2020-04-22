b08648b085c495833f27ed68c6772fc73b009921
Elnatan Torres | Stone <etorres@stone.com.br>
2020-04-21 23:07:52 -0300

    Add IrrigationSystem table

--- AutomatedIrrigationSystemDatabase/dbo/Tables/IrrigationExecution.sql
+++ AutomatedIrrigationSystemDatabase/dbo/Tables/IrrigationExecution.sql
@@ -1,19 +1,11 @@
 ﻿CREATE TABLE [dbo].[IrrigationExecution] (
     [Id]                       BIGINT   IDENTITY (1, 1) NOT NULL,
-    [PlantationId]             INT      NOT NULL,
-    [IrrigatorId]              INT      NOT NULL,
-    [ControllerId]             INT      NOT NULL,
-    [SolenoidValveId]          INT      NOT NULL,
-    [SensorId]                 INT      NOT NULL,
+    [IrrigationSystemId]       INT      NOT NULL,
     [InitialExecutionDateTime] DATETIME NOT NULL,
     [FinalExecutionDateTime]   DATETIME NULL,
     [UserId]                   BIGINT   NOT NULL,
     CONSTRAINT [pk_IrrigationExecution] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [fk_IrrigationExecution_Controller] FOREIGN KEY ([ControllerId]) REFERENCES [dbo].[Controller] ([Id]),
-    CONSTRAINT [fk_IrrigationExecution_Irrigator] FOREIGN KEY ([IrrigatorId]) REFERENCES [dbo].[Irrigator] ([Id]),
-    CONSTRAINT [fk_IrrigationExecution_Plantation] FOREIGN KEY ([PlantationId]) REFERENCES [dbo].[Plantation] ([Id]),
-    CONSTRAINT [fk_IrrigationExecution_Sensor] FOREIGN KEY ([SensorId]) REFERENCES [dbo].[Sensor] ([Id]),
-    CONSTRAINT [fk_IrrigationExecution_SolenoidValve] FOREIGN KEY ([SolenoidValveId]) REFERENCES [dbo].[SolenoidValve] ([Id]),
+    CONSTRAINT [fk_IrrigationExecution_IrrigationSystem] FOREIGN KEY ([IrrigationSystemId]) REFERENCES [dbo].[IrrigationSystem] ([Id]),
     CONSTRAINT [fk_IrrigationExecution_User] FOREIGN KEY ([UserId]) REFERENCES [auth].[User] ([Id])
 );
 
---
+++ AutomatedIrrigationSystemDatabase/dbo/Tables/IrrigationSystem.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[IrrigationSystem] (
+    [Id]                       INT   IDENTITY (1, 1) NOT NULL,
+    [PlantationId]             INT      NOT NULL,
+    [IrrigatorId]              INT      NOT NULL,
+    [ControllerId]             INT      NOT NULL,
+    [SolenoidValveId]          INT      NOT NULL,
+    [SensorId]                 INT      NOT NULL
+    CONSTRAINT [pk_IrrigationSystem] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [fk_IrrigationSystem_Controller] FOREIGN KEY ([ControllerId]) REFERENCES [dbo].[Controller] ([Id]),
+    CONSTRAINT [fk_IrrigationSystem_Irrigator] FOREIGN KEY ([IrrigatorId]) REFERENCES [dbo].[Irrigator] ([Id]),
+    CONSTRAINT [fk_IrrigationSystem_Plantation] FOREIGN KEY ([PlantationId]) REFERENCES [dbo].[Plantation] ([Id]),
+    CONSTRAINT [fk_IrrigationSystem_Sensor] FOREIGN KEY ([SensorId]) REFERENCES [dbo].[Sensor] ([Id]),
+    CONSTRAINT [fk_IrrigationSystem_SolenoidValve] FOREIGN KEY ([SolenoidValveId]) REFERENCES [dbo].[SolenoidValve] ([Id])
+);
+
