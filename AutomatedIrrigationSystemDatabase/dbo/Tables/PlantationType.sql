97542d5e990c9e0cbf19c3394b3b7ec111c388b1
Elnatan Torres | Stone <etorres@stone.com.br>
2020-04-21 23:12:38 -0300

    Add PlantationType table

---
+++ AutomatedIrrigationSystemDatabase/dbo/Tables/PlantationType.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[PlantationType] (
+    [Id]   INT           IDENTITY (1, 1) NOT NULL,
+    [Name] VARCHAR (100) NOT NULL,
+    CONSTRAINT [pk_PlantationType] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
